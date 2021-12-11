33f210b8a1334fe1603de5dd8edddb47a91c4213
ahmedbhesaniya97 <ahmed@zujo.io>
2021-12-11 13:34:56 +0530

    Imrove Wordpress docs.

--- sidebars.js
+++ sidebars.js
@@ -199,7 +199,6 @@ module.exports = {
         "guide/video-and-audio-calling-api-sdk/features/mic-controls",
         "guide/video-and-audio-calling-api-sdk/features/screenshare",
 
-        
         "guide/video-and-audio-calling-api-sdk/features/toggle-participant-media",
         "guide/video-and-audio-calling-api-sdk/features/send-messages",
         "guide/video-and-audio-calling-api-sdk/features/record-meeting",
@@ -568,6 +567,7 @@ module.exports = {
           items: [
             "tutorials/realtime-communication/prebuilt-sdk/quickstart-prebuilt-js",
             "tutorials/realtime-communication/js-sdk/quickstart-js",
+            "tutorials/realtime-communication/prebuilt-sdk/quickstart-prebuilt-wordpress",
           ],
         },
       ],
