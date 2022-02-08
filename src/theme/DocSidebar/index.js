a69c2312b6d99211fcb6a089b2c432e2fcc47fe6
Rajan Surani <rajansurani14@gmail.com>
2022-02-08 11:37:35 +0530

    build fixsidebar converted to component

--- src/theme/DocSidebar/index.js
+++ src/theme/DocSidebar/index.js
@@ -16,17 +16,10 @@ import {
 } from "@docusaurus/theme-common";
 import Logo from "@theme/Logo";
 import IconArrow from "@theme/IconArrow";
-import BrowserOnly from "@docusaurus/BrowserOnly";
 import { translate } from "@docusaurus/Translate";
 import DocSidebarItems from "@theme/DocSidebarItems";
 import styles from "./styles.module.css";
-import react_versions from '../../../react_docs_versions.json';
-import android_versions from '../../../android_docs_versions.json';
-import react_native_versions from '../../../react_native_docs_versions.json';
-import flutter_versions from '../../../flutter_docs_versions.json';
-import ios_versions from '../../../ios_docs_versions.json';
-import javascript_versions from '../../../javascript_docs_versions.json';
-import prebuilt_versions from '../../../prebuilt_docs_versions.json';
+import SidebarDropdowns from "../SidebarDropdowns";
 
 function useShowAnnouncementBar() {
   const { isActive } = useAnnouncementBar();
@@ -73,70 +66,6 @@ function DocSidebarDesktop({ path, sidebar, onCollapse, isHidden }) {
     navbar: { hideOnScroll },
     hideableSidebar,
   } = useThemeConfig();
-  const [sdk, setSDK] = useState();
-  const [version, setVersion] = useState("");
-  const [versionList, setVersionList] = useState([])
-
-  function routingSDK(e) {
-    setSDK(e.target.value);
-    window.location.assign("http://" + window.location.host + "/"+e.target.value+"/guide/video-and-audio-calling-api-sdk/getting-started");
-  }
-
-  function routeVersion(e) {
-    var currentPath = window.location.pathname;
-    var currentVersion = currentPath.split("/")[2];
-    
-    if (currentVersion.match('([0-9])+\.([0-9])+\.([0-9]|[a-z])+')){
-      currentPath = currentPath.replace(
-        currentPath.split("/")[2],
-        e.target.value == versionList[0] ? "": e.target.value
-      );
-      currentPath = currentPath.replace('//', "/");
-    }
-    else {
-      currentPath = currentPath.replace(
-        currentPath.split("/")[1],
-        (sdk+"/"+e.target.value)
-      )
-    }
-    console.log()
-    window.location.replace("http://" + window.location.host + currentPath);
-  }
-
-  useEffect(() => {
-    var currentSdk = window.location.pathname.split('/')[1];
-    var currentVersion = window.location.pathname.split("/")[2];
-    if (currentVersion.match('([0-9])+\.([0-9])+\.([0-9]|[a-z])+'))
-      setVersion(currentVersion)
-    else {
-      setVersion(versionList[0])
-    }
-    if (currentSdk == "react") {
-      setVersionList(react_versions)
-    } else if (currentSdk == "javascript") {
-      setVersionList(javascript_versions)
-    }
-     else if (currentSdk == "react-native") {
-      
-      setVersionList(react_native_versions)
-    }
-     else if (currentSdk == "android") {
-      
-      setVersionList(android_versions)
-    }
-     else if (currentSdk == "ios") {
-      
-      setVersionList(ios_versions)
-    }
-     else if (currentSdk == "flutter") {
-      
-      setVersionList(flutter_versions)
-    } else if (currentSdk == "prebuilt") {
-      
-      setVersionList(prebuilt_versions)
-    }
-    setSDK(currentSdk);
-  },[])
 
   return (
     <div
@@ -152,29 +81,7 @@ function DocSidebarDesktop({ path, sidebar, onCollapse, isHidden }) {
           [styles.menuWithAnnouncementBar]: showAnnouncementBar,
         })}
       >
-        <BrowserOnly>{()=>
-        <div className="row">
-        {sdk != "docs" && sdk !="prebuilt" ? (
-            <select
-              onChange={routingSDK}
-              defaultValue={sdk}
-              className="col dropdown--hoverable"
-            >
-              <option value="react">React</option>
-              <option value="javascript">JS</option>
-              <option value="react-native">React Native</option>
-              <option value="android">Android</option>
-              <option value="ios">IOS</option>
-              <option value="flutter">Flutter</option>
-            </select>
-          ) : null}
-          {sdk != "docs"? (<select className="col dropdownSidebar" value={version}onChange={routeVersion}>
-                {versionList.map((v) => {
-                return <option value={v}>{v}</option>
-              })}
-            </select>):null}
-          </div>}
-          </BrowserOnly>
+        <SidebarDropdowns/>
         <ul className={clsx(ThemeClassNames.docs.docSidebarMenu, "menu__list")}>
           <DocSidebarItems items={sidebar} activePath={path} level={1} />
         </ul>
@@ -185,6 +92,8 @@ function DocSidebarDesktop({ path, sidebar, onCollapse, isHidden }) {
 } // eslint-disable-next-line react/function-component-definition
 
 const DocSidebarMobileSecondaryMenu = ({ toggleSidebar, sidebar, path }) => (
+  <div>
+  <SidebarDropdowns/>
   <ul className={clsx(ThemeClassNames.docs.docSidebarMenu, "menu__list")}>
     <DocSidebarItems
       items={sidebar}
@@ -201,7 +110,8 @@ const DocSidebarMobileSecondaryMenu = ({ toggleSidebar, sidebar, path }) => (
       }}
       level={1}
     />
-  </ul>
+    </ul>
+    </div>
 );
 
 function DocSidebarMobile(props) {
