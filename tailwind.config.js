261c589dfaed2406d70b746af3f3d89a5ca92c62
BhumiSalat <bhumisalat98@gmail.com>
2022-03-05 12:10:09 +0530

    WIP: home page design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,46 +1,59 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  mode: "jit",
+  purge: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
   theme: {
-    // extends: {
-    colors: {
-      green: {
-        600: "#053016",
-        700: "#72C894",
-        500: "#0F9143",
+    extends: {
+      screens: {
+        sm: "640px",
+        // => @media (min-width: 640px) { ... }
+
+        md: "768px",
+        // => @media (min-width: 768px) { ... }
+
+        lg: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        xl: "1280px",
+        // => @media (min-width: 1280px) { ... }
+
+        "2xl": "1536px",
+        // => @media (min-width: 1536px) { ... }
       },
-      slate: {
-        900: "#1e1e1",
+      colors: {
+        gray: {
+          100: "#eeeeee",
+          200: "#e0e0e0",
+          300: "#bbbbbb",
+          400: "#666666",
+          500: "#444444",
+          600: "#2a2a2a",
+          700: "#1e1e1e",
+          750: "#1e1e1e",
+          800: "#181818",
+          900: "#0f0f0f",
+        },
+        green: {
+          600: "#053016",
+          700: "#72C894",
+          500: "#0F9143",
+        },
+        slate: {
+          900: "#1e1e1e",
+        },
+        // black: "#000000",
       },
-      black: "#000000",
-    },
-    fontFamily: {
-      sans: ["lato", "office code pro"],
-      serif: ["lato", "office code pro"],
-      mono: ["lato", "office code pro"],
-    },
-    fontSize: {
-      xs: ".72rem",
-      sm: "16px",
-      px18: "18px",
-      md: "24px",
-      px28: "28px",
-      px32: "32px",
-      lg: "36px",
-      xl: "50px",
-    },
-    screens: {
-      xs: { min: "230px", max: "639px" },
-      sm: { min: "640px", max: "780px" },
-      // => @media (min-width: 640px and max-width: 767px) { ... }
-      md: { min: "781px", max: "1023px" },
-      // => @media (min-width: 768px and max-width: 1023px) { ... }
-      lg: { min: "1024px", max: "1279px" },
-      // => @media (min-width: 1024px and max-width: 1279px) { ... }
-      xl: { min: "1280px", max: "1535px" },
-      // => @media (min-width: 1280px and max-width: 1535px) { ... }
-      xl2: { min: "1536px" },
+      fontFamily: {
+        sans: ["lato", "office code pro"],
+        serif: ["lato", "office code pro"],
+        mono: ["lato", "office code pro"],
+      },
     },
   },
-  darkMode: "class",
-  plugins: [],
+
+  plugins: [require("tailwindcss/colors")],
 };
