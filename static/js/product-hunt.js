caacb55d6161d6c6564ad9be149f7ed126debfe8
ChintanRajpara <chintan@zujo.io>
2022-09-15 17:15:09 +0530

    server default running port changed

---
+++ static/js/product-hunt.js
@@ -0,0 +1,18 @@
+var $productHunt = $productHunt || {};
+$productHunt = $productHunt || {
+  appId: 194253,
+  position: "bottomLeft",
+  ready: function () {},
+};
+window.productHuntUpcoming = {
+  appId: 194253,
+  position: "bottomLeft",
+};
+var doc = document;
+a = doc.createElement("script");
+a.type = "text/javascript";
+a.id = "ph-script";
+a.defer = true;
+a.src = "https://assets.producthunt.com/assets/upwigloader.js";
+b = doc.getElementsByTagName("script")[0];
+b.parentNode.insertBefore(a, b);
