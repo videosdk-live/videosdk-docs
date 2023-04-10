1ad81255b8d08bc1e120199183ee7ac51604769d
Ahmed Bhesaniya <ahmed@videosdk.live>
2023-04-10 11:10:54 +0530

    rename server to server side in dropdown

--- src/theme/SidebarDropdowns.js
+++ src/theme/SidebarDropdowns.js
@@ -136,7 +136,7 @@ export default function SidebarDropdowns() {
     },
     {
       id: "api-reference",
-      value: "Server",
+      value: "Server Side",
       icon: "/img/icons/libraries/server.svg",
     },
   ];
