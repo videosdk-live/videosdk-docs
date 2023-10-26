8f75aae6df85d5d46bbd11cf168dc8e4a0914603
BhumiSalat <bhumisalat98@gmail.com>
2023-10-26 10:30:41 +0530

    replaceSearchResultPathname param passed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,7 +45,7 @@ module.exports = {
       contextualSearch: true,
       searchParameters: {},
       replaceSearchResultPathname: {
-        from: "/docs",
+        from: "/docs/",
         to: "/search",
       },
     },
