56c8b9f32518f67824083d7b0000f57372f4a71b
Bhumi Salat <bhumisalat98@gmail.com>
2024-01-31 17:17:52 +0530

    document.js file added

---
+++ src/pages/_document.js
@@ -0,0 +1,27 @@
+// pages/_document.js
+
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head></Head>
+        <body>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-P7SM6MDR"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
